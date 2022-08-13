10b4331f7bbc08c140f2a367fefdf1e1f7808d27
SniperDitto <sniperditto@kakao.com>
2022-08-13 22:10:12 +0900

    generate project

---
+++ chatTest/src/main/kotlin/com/example/chatTest/ChatTestApplication.kt
@@ -0,0 +1,11 @@
+package com.example.chatTest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ChatTestApplication
+
+fun main(args: Array<String>) {
+	runApplication<ChatTestApplication>(*args)
+}
